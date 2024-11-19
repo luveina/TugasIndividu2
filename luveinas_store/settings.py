6702f823213f326c45d52d1c70e705c153d4fe99
luveina <saragihluvenia@gmail.com>
2024-11-19 23:41:45 +0700

    menyelesaikan Tugas Individu 9

--- luveinas_store/settings.py
+++ luveinas_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)6@qg3uagk8he054pv8g5rtx^+ar-m5_$%%cv+i9x6d+#yk3@h
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "luvenia-feodora-luveinasstore.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "luvenia-feodora-luveinasstore.pbp.cs.ui.ac.id", "10.0.2.2", "127.0.0.1:8000"]
 
 
 # Application definition
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'luveinas_store.urls'
@@ -130,3 +133,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
